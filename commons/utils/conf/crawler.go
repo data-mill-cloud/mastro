cdf1a1b8b8f9a890359de4d3295bbc4b3fe02125
pilillo <>
2021-10-11 18:30:26 +0200

    edited crawler conf

--- commons/utils/conf/crawler.go
+++ commons/utils/conf/crawler.go
@@ -2,41 +2,9 @@ package conf
 
 // CrawlerDefinition ... Config for a Crawler service
 type CrawlerDefinition struct {
-	Root              string `yaml:"root"`
-	FilterFilename    string `yaml:"filter-filename"`
-	ScheduleEvery     Period `yaml:"schedule-period"`
-	ScheduleValue     uint64 `yaml:"schedule-value"`
-	StartNow          bool   `yaml:"start-now"`
-	CatalogueEndpoint string `yaml:"catalogue-endpoint"`
+	CatalogueEndpoint string  `yaml:"catalogue-endpoint"`
+	Root              string  `yaml:"root"`
+	FilterFilename    string  `yaml:"filter-filename"`
+	Schedule          *string `yaml:"schedule,omitempty"`
+	StartNow          *bool   `yaml:"start-now,omitempty"`
 }
-
-// Period ... time period to schedule the crawler for
-type Period string
-
-const (
-	// Seconds ... time interval
-	Seconds Period = "seconds"
-	// Minutes ... schedule time interval
-	Minutes = "minutes"
-	// Hours ... schedule time interval
-	Hours = "hours"
-	// Days ... schedule time interval
-	Days = "days"
-	// Weeks ... schedule time interval
-	Weeks = "weeks"
-
-	// Monday ...
-	Monday = "monday"
-	// Tuesday ...
-	Tuesday = "tuesday"
-	// Wednesday ...
-	Wednesday = "wednesday"
-	// Thursday ...
-	Thursday = "thursday"
-	// Friday ...
-	Friday = "friday"
-	// Saturday ...
-	Saturday = "saturday"
-	// Sunday = "sunday"
-	Sunday = "sunday"
-)
