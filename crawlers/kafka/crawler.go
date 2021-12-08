8f62961459e1d10ba9629e040f7348b0f810da1b
pilillo <>
2021-12-08 16:57:08 +0100

    added filter to kafka crawler

--- crawlers/kafka/crawler.go
+++ crawlers/kafka/crawler.go
@@ -3,6 +3,7 @@ package kafka
 import (
 	"fmt"
 	"log"
+	"regexp"
 
 	"github.com/data-mill-cloud/mastro/commons/abstract"
 	"github.com/data-mill-cloud/mastro/commons/sources/kafka"
@@ -36,31 +37,32 @@ func (crawler *kafkaCrawler) WalkWithFilter(root string, filter string) ([]abstr
 
 	var assets []abstract.Asset
 	for _, m := range metadata.Topics {
-		streamInfo, err := abstract.GetStreamInfoByName(m.Topic)
-		if err != nil {
-			return nil, err
-		}
-
-		schemaVersions, err := crawler.connector.SchemaRegistryClient.GetSchemaVersions(m.Topic)
-		if err != nil {
-			// skip without failing the entire process
-			log.Printf("couldn't get schema versions for topic %s: %v", m.Topic, err)
-		} else {
-			for _, schemaVersion := range schemaVersions {
-				schema, err := crawler.connector.SchemaRegistryClient.GetSchemaByVersion(m.Topic, schemaVersion)
-				if err != nil {
-					return nil, fmt.Errorf("couldn't get schema for topic %s: %v", m.Topic, err)
-				}
-				streamInfo.Schema[schema.Version()] = schema.Schema()
+		if matchesFilter, _ := regexp.MatchString(filter, m.Topic); matchesFilter {
+			streamInfo, err := abstract.GetStreamInfoByName(m.Topic)
+			if err != nil {
+				return nil, err
 			}
-		}
 
-		a, err := streamInfo.BuildAsset()
-		if err != nil {
-			return nil, err
+			schemaVersions, err := crawler.connector.SchemaRegistryClient.GetSchemaVersions(m.Topic)
+			if err != nil {
+				// skip without failing the entire process
+				log.Printf("couldn't get schema versions for topic %s: %v", m.Topic, err)
+			} else {
+				for _, schemaVersion := range schemaVersions {
+					schema, err := crawler.connector.SchemaRegistryClient.GetSchemaByVersion(m.Topic, schemaVersion)
+					if err != nil {
+						return nil, fmt.Errorf("couldn't get schema for topic %s: %v", m.Topic, err)
+					}
+					streamInfo.Schema[schema.Version()] = schema.Schema()
+				}
+			}
+
+			a, err := streamInfo.BuildAsset()
+			if err != nil {
+				return nil, err
+			}
+			assets = append(assets, *a)
 		}
-		assets = append(assets, *a)
 	}
-
 	return assets, nil
 }
