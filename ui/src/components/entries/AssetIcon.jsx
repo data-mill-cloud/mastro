b4de21f552215b92683358e09429d8cdd3b494bb
pilillo <>
2022-01-15 02:24:41 +0100

    .

--- ui/src/components/entries/AssetIcon.jsx
+++ ui/src/components/entries/AssetIcon.jsx
@@ -1,18 +1,23 @@
 import {VscTable} from 'react-icons/vsc';
 import {FcIdea} from 'react-icons/fc';
 import {BiCube} from 'react-icons/bi';
-import {FaStream} from 'react-icons/fa';
+import {FaStream, FaRobot} from 'react-icons/fa';
 import {ImDatabase} from 'react-icons/im';
 import {BsSpeedometer2} from 'react-icons/bs';
+import {GiTeePipe} from 'react-icons/gi';
 
 const getIcon = (type, size = "5xl") => {
     switch (type) {
+        case 'model':
+            return <FaRobot className={`text-${size}`} />;
         case 'metricset':
             return <BsSpeedometer2 className={`text-${size}`} />;
         case 'featureset':
             return <FcIdea className={`text-${size}`} />;
         case 'stream':
             return <FaStream className={`text-${size}`} />;
+        case 'pipeline':
+            return <GiTeePipe className={`text-${size}`} />;
         case 'table':
             return <VscTable className={`text-${size}`} />;
         case 'db':
